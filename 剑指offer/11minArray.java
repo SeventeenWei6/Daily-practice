d3d066f0dc2ff9afb0894dbc9fd7253c4a5450d0
SeventeenWei6 <405864154@qq.com>
2020-05-21 10:31:45 +0800

    面试题11旋转数组的最小数字

---
+++ 剑指offer/11minArray.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minArray(int[] numbers) {
+        int i = 0,j = numbers.length-1;
+        
+        while(i!=j)
+        {
+            int m  = (i+j)/2;
+            if(numbers[m]<numbers[j])
+            {
+                j = m;
+            }
+            else if(numbers[m]>numbers[j]){
+                i = m+1;
+            }
+            else{
+                j--;
+            }
+        }
+        return  numbers[i];
+    }
+}
